865bfa9fea9dc9e5d247d3710458ef06ff52d913
Beni Samuel <benisamuel566@gmail.com>
2024-11-06 16:04:17 +0200

    Added or

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Registration from "./Pages/Registration/Registration";
+import Login from "./Pages/Photograph/Login/Login";
+import Signup from "./Pages/Photograph/Signup/Signup";
 
 
 const App = () => {
@@ -7,6 +9,9 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" Component={Registration}/>
+        <Route path="/photographer/login" Component={Login} />
+        <Route path="/photographer/signup" Component={Signup} />
+        
       </Routes>
     </Router>
   )
--- src/Components/Registration/RegCenter.tsx
+++ src/Components/Registration/RegCenter.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const RegCenter:React.FC = () => {
   return (
     <div className=' absolute left-[30%] top-[20%] bg-[#171717] h-80 w-[30rem] rounded-xl flex flex-col items-center gap-12 py-8'>
         <div className=' text-white font-mulish font-bold text-lg'> Sign up as </div>
-        <div className=' flex flex-col gap-3'>
+        <div className=' flex flex-col gap-3 '>
             <button className=' bg-[#616161] text-white  py-3 rounded-[6px] font-poppins'>Client</button>
-            <button className=' bg-[#616161] text-white px-32 py-3 rounded-[6px] font-poppins'>Photographer</button>
+            <div className=' flex flex-row items-center gap-10'>
+                <div className=' bg-white h-[0.01rem] w-32'/>
+                <div className=' text-white font-mulish font-bold'>Or</div>
+                <div className=' bg-white h-[0.01rem] w-32'/>
+            </div>
+            <button className=' bg-[#616161] text-white px-32 py-3 rounded-[6px] font-poppins'> <Link to="/photographer/login"> Photographer</Link></button>
         </div>
     </div>
   )
