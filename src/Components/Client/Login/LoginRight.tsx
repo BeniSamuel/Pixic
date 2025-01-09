45c443e90b88e9898043207fadcffb6065f29776
Beni Samuel <149045020+BeniSamuel@users.noreply.github.com>
2025-01-09 19:38:23 +0200

    Update LoginRight.tsx

--- src/Components/Client/Login/LoginRight.tsx
+++ src/Components/Client/Login/LoginRight.tsx
@@ -6,9 +6,9 @@ const LoginRight:React.FC = () => {
     <div className=' bg-[#0E0E0E] h-[100vh] w-[100%] md:w-[70%] flex flex-col items-center gap-16 justify-center'>
         <div className=' text-[#470407] font-mulish text-2xl font-extrabold'>PiXic</div>
         <div className=' flex flex-col gap-9 items-center'>
-            <input type="email" placeholder='Email...'  className=' outline-none bg-transparent font-poppins text-white placeholder:font-poppins bg-[#1B1A1A] border-[#505050] border-2 rounded-lg pl-7 py-3 w-[30rem]'/>
+            <input type="email" placeholder='Email'  className=' outline-none bg-transparent font-poppins text-white placeholder:font-poppins bg-[#1B1A1A] border-[#505050] border-2 rounded-lg pl-7 py-3 w-[30rem]'/>
             <div className=' flex flex-col gap-2'>
-                <input type="password" placeholder='Password...' className=' outline-none bg-transparent font-poppins text-white placeholder:font-poppins bg-[#1B1A1A] border-[#505050] border-2 rounded-lg pl-7 py-3 w-[30rem]'/>
+                <input type="password" placeholder='Password' className=' outline-none bg-transparent font-poppins text-white placeholder:font-poppins bg-[#1B1A1A] border-[#505050] border-2 rounded-lg pl-7 py-3 w-[30rem]'/>
                 <p className=' text-right text-white font-poppins'>Forgot Password?</p>
             </div>
         </div>
