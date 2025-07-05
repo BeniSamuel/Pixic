a565b5eb3e49a1ae1ed698e79002cca35ac60ec7
benisamuel <benisamuel566@gmail.com>
2025-07-05 19:54:04 +0200

    added calendar page

---
+++ src/Pages/Photographer/Calendar/Calendar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DashboardMain from "../../../Layout/DashboardMain";
+
+const Calendar: React.FC = () => {
+  return (
+    <DashboardMain>
+      <div>Calendar</div>
+    </DashboardMain>
+  );
+};
+
+export default Calendar;
