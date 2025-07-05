e1a9cfaf2aee1f5952d996e105379e79aebd60d3
benisamuel <benisamuel566@gmail.com>
2025-07-05 19:53:22 +0200

    added profile page

---
+++ src/Pages/Photographer/Profile/Profile.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DashboardMain from "../../../Layout/DashboardMain";
+
+const Profile: React.FC = () => {
+  return (
+    <div>
+      <DashboardMain>
+        <div>Hello</div>
+      </DashboardMain>
+    </div>
+  );
+};
+
+export default Profile;
