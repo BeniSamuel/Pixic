3663c389c48a0b2e10e8b11dc16fe52ad3c3c034
benisamuel <benisamuel566@gmail.com>
2025-07-05 19:53:42 +0200

    added chat page

---
+++ src/Pages/Photographer/Chat/Chat.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DashboardMain from "../../../Layout/DashboardMain";
+
+const Chat: React.FC = () => {
+  return (
+    <DashboardMain>
+      <div>chattt</div>
+    </DashboardMain>
+  );
+};
+
+export default Chat;
